5446e578c8d65466a3e668d3fa4e99879062a6be
cmonladies <cmonladies@gmail.com>
2017-01-11 02:28:55 +0300

    part1

--- les-4 Streams/index.js
+++ les-4 Streams/index.js
@@ -5,14 +5,12 @@ hash.setEncoding('hex');
 const input = fs.createReadStream('text');
 const out = fs.createWriteStream('out');
 
-hash.on('readable', () => {
-  var data = hash.read();
-  if (data)
-    console.log(data.toString('hex'));
-});
 
-//input.pipe(hash); //Если только так, в консоль пишет
-//input.pipe(hash).pipe(out); //Если так - только в файл. Вопрос!
-input.pipe(hash).pipe(process.stdout);
-hash.pipe(out);
-//Вопрос! Почему при цепочке стримов input -> hash -> out не срабатывает hash.on('readable' .... ?
+hash.on('data', function(chunk) {
+  console.log('HeX:', chunk);
+});
+input.pipe(hash).pipe(out);
+//Или так:
+//input.pipe(hash).pipe(process.stdout);
+//hash.pipe(out);
+
