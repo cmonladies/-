297785256495f8a26a77c27ac85c8dc3a0de5c44
cmonladies <cmonladies@gmail.com>
2017-01-11 03:56:09 +0300

    part2 done

---
+++ les-4 Streams/HashTransformClass.js
@@ -0,0 +1,20 @@
+'use strict';
+const Transform = require('stream').Transform;
+const hash = require('crypto').createHash('md5');
+
+
+class HashTransform extends Transform {
+  constructor(options) {
+    super(options);
+  }
+}
+
+HashTransform.prototype._transform = function (data, encoding, callback) {
+  let encrypted = hash.update(data).digest('hex');
+  console.log('Part2:',encrypted);
+  this.push(encrypted);
+  callback();
+};
+
+
+module.exports = HashTransform;
--- les-4 Streams/index.js
+++ les-4 Streams/index.js
@@ -1,16 +1,28 @@
-const fs = require('fs');
-const crypto = require('crypto');
-const hash = crypto.createHash('md5');
-hash.setEncoding('hex');
-const input = fs.createReadStream('text');
-const out = fs.createWriteStream('out');
+'use strict';
 
+const fs = require('fs');
+const hash = require('crypto').createHash('md5').setEncoding('hex');
+const input = fs.createReadStream('text');
+const out = fs.createWriteStream('outPart1');
+
+//part 1
+/***********************************************/
 
 hash.on('data', function(chunk) {
-  console.log('HeX:', chunk);
+  console.log('Part1, HeX:', chunk);
 });
 input.pipe(hash).pipe(out);
 //Или так:
 //input.pipe(hash).pipe(process.stdout);
 //hash.pipe(out);
 
+
+//part2
+/***********************************************/
+
+const HashTransform = require('./HashTransformClass.js');
+const out2 = fs.createWriteStream('outPart2');
+
+var hashTransform = new HashTransform;
+
+input.pipe(hashTransform).pipe(out2);
