7215fb8bd542b608e1b6199b8483e987be0f45d6
cmonladies <cmonladies@gmail.com>
2017-04-19 21:33:23 +0300

    - dry

--- mocha/test/PokemonClassTest.js
+++ mocha/test/PokemonClassTest.js
@@ -4,29 +4,29 @@ const Pokemon = require('../pokemons/pokemonClass.js');
 const PokemonList = require('../pokemons/PokemonListClass.js')
 
 
-describe('Тестирование класса Pokemon:', () => {
+
+describe('Pokemon class tests:', () => {
 
   describe('Method: show()', () => {
 
     var tests = [
+    {poke: new Pokemon('test2',77), expected: "My name is test2. My level is 77!"},
     {poke: new Pokemon(), expected: "My name is undefined. My level is undefined!"},
-    {poke: new Pokemon('test1'), expected: "My name is test1. My level is undefined!"},
-    {poke: new Pokemon('test2',77), expected: "My name is test2. My level is 77!"}];
+    {poke: new Pokemon('testPoke','seven'), expected: "My name is testPoke. My level is seven!"}];
 
-
-    tests.forEach((pokemon,number) => {
-      it(`Вернет информацию о покемоне, созаднном при ${number} аргументах` , () => {
-          let showText = pokemon.poke.show();
-
-          expect(showText).to.equal(pokemon.expected);
+      it(`Тестируем метод для экземпляра класса Pokemon созданного при передаче в конструктор значений test2 и 77, вернет:My name is test2. My level is 77!` , () => {
+          let showText = tests[0].poke.show();
+          expect(showText).to.equal(tests[0].expected);
       });
-    });
 
-    it('Вернет текстовое значение уровня покемона в строке информации', () => {
-      let testPokemon = new Pokemon ('testPoke', 'seven');
-      let showText = testPokemon.show();
+      it(`Тестируем метод для экземпляра класса Pokemon созданного без параметров name и level, вернет: My name is undefined. My level is undefined!` , () => {
+          let showText = tests[1].poke.show();
+          expect(showText).to.equal(tests[1].expected);
+      });
 
-      expect(showText,'Ошибочное значение').to.equal("My name is testPoke. My level is seven!");
+      it('Создаем покемона с параметрами name = testPoke и level = "seven", метод должен вернуть строку: My name is testPoke. My level is seven!', () => {
+          let showText = tests[2].poke.show();
+        expect(showText,'Ошибочное значение').to.equal(tests[2].expected);
     });
   });
 });
